e628187b9529f986bd7fc3f6299700491f309b3b
kohlmu-pivotal <ukohlmeyer@pivtotal.io>
2019-05-14 17:18:44 -0700

    Initial vanilla implementation

---
+++ src/main/java/io/pivotal/pcc/demo/client/ClientAuthentication.java
@@ -0,0 +1,15 @@
+package io.pivotal.pcc.demo.client;
+
+import java.util.Properties;
+
+import org.apache.geode.distributed.DistributedMember;
+import org.apache.geode.security.AuthInitialize;
+import org.apache.geode.security.AuthenticationFailedException;
+
+public class ClientAuthentication implements AuthInitialize {
+
+	@Override public Properties getCredentials(Properties properties, DistributedMember distributedMember, boolean b)
+		throws AuthenticationFailedException {
+		return properties;
+	}
+}
